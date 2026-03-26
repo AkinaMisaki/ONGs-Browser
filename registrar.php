2df14dee057ac42656d5ee06cf2579c80bc7ec0b
HenrySigx <luizhporto123@gmail.com>
2026-03-26 20:55:26 -0300

    Primeira versão do controle de senha

---
+++ registrar.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página de Prática</title>
+    
+    <link rel="stylesheet" href="css/login_usuario.css">
+</head>
+<body>
+
+    <header class="barra-fixa">
+        <nav>
+            <button onclick="window.location.href='../index.php'">Início</button>
+            <button onclick="window.location.href='sobre.html'">Sobre</button>
+            <button onclick="window.location.href='contato.html'">Contato</button>
+        </nav>
+    </header>
+
+    <main>
+        <h1>Criação de Usuário</h1>
+        
+        <div id="mensagem-alerta"></div>
+
+        <form id="formContato">
+            <label for="user_name">Nome:</label>
+            <input type="text" id="new_user_name" name="userName" placeholder="Digite seu nome">
+
+            <label for="user_name">E-mail:</label>
+            <input type="email" id="new_user_email" name="userEmail" placeholder="Digite seu email">
+
+            <label for="user_acess">Novo Usuário:</label>
+            <input type="text" id="new_user_acess" name="userAcess" placeholder="Digite seu usuário">
+
+            <label for="passCheck">Nova Senha:</label>
+            <input type="password" id="new_passCheck" name="password" placeholder="Digite sua senha">
+            <strong>Lembre-se, sua senha deve ser composta por 8 caracteres, pelo menos 1 letra maiuscula, 1 minuscula e 1 número!</strong>
+            <button type="button" onclick="realizarCadastroUsuario()">Cadastrar</button>
+        </form>
+    </main>
+
+    <script src="../js/registro.js"></script>
+</body>
+</html>
