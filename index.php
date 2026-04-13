8a03b2129694926f01fd767338b530d8e547311b
Gabriel <bieleubolacha@gmail.com>
2026-04-13 20:39:00 -0300

    corrige caminho do include do config.php no index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include __DIR__ . '/config.php';
+include __DIR__ . '/conn/config.php';
 
 $sql = "SELECT id_ong, nome_ong, descricao FROM ong ORDER BY RAND() LIMIT 20";
 $result = $conn->query($sql);
